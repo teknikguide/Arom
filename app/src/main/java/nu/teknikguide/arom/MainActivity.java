e43dbd860cd46e6a91310fbee1d8d0c1f970aad0
Teknikguide <anders@teknikguide.nu>
2023-10-08 14:25:29 +0200

    Android kurs start

---
+++ app/src/main/java/nu/teknikguide/arom/MainActivity.java
@@ -0,0 +1,31 @@
+package nu.teknikguide.arom;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView startersCard;
+    CardView mainsCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startersCard = findViewById(R.id.card_view_starters);
+        mainsCard = findViewById(R.id.card_view_mains);
+
+
+        startersCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startersActivityIntent = new Intent(MainActivity.this, StarterActivity.class);
+                startActivity(startersActivityIntent);
+            }
+        });
+    }
+}
